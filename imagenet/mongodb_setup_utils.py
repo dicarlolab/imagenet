ca735f4e03c5c5f6f1f298b21ea5b2d317ca564e
Diego Ardila <ardila@mit.edu>
2013-09-16 16:41:00 -0400

    Some code used to set up mongodb on dicarlo5

--- imagenet/mongodb_setup_utils.py
+++ imagenet/mongodb_setup_utils.py
@@ -7,6 +7,7 @@ from joblib import Parallel, delayed
 import pymongo as pm
 import gridfs
 
+
 def get_tar_filenames(path=None):
     if path is None:
         path = os.getcwd()
@@ -16,25 +17,47 @@ def get_tar_filenames(path=None):
 db = pm.MongoClient('dicarlo5').gridfs_example
 default_fs = gridfs.GridFS(db)
 
+
 def put_tar_file_contents_on_gridfs(tar_file_name, fs=default_fs, force=True):
+    print 'uploading '+tar_file_name
     tar_file = tarfile.open(tar_file_name, mode='r|')
     filenames = []
     for member in tar_file:
-        file = tar_file.extractfile(member)
+        file_obj = tar_file.extractfile(member)
         filename = member.name
         filenames.append(filename)
         if force and fs.exists(filename):
             fs.delete(filename)
-            print 'Overwriting '+ filename
-        fs.put(file, _id=filename)
+            print 'Overwriting '+filename
+        fs.put(file_obj, _id=filename)
     return filenames
 
 
 def parallel_upload_tar_folder(path=None, n_jobs=-1):
+    """
+    Utility to upload contents of many tarballs to the default gridfs
+    :param path: Path to folder full of tars to upload
+    :param n_jobs: Number of processes to use
+    :return: returns tar filenames uplaoded and a list of lists of filenames (one per tar)
+    """
     if path is None:
         path = os.getcwd()
     tar_filenames = get_tar_filenames(path)
     ids = Parallel(n_jobs=n_jobs)(delayed(put_tar_file_contents_on_gridfs)(tar_filename)
-                                      for tar_filename in tar_filenames)
+                                  for tar_filename in tar_filenames)
     return tar_filenames, ids
 
+
+#took about 4 hours with all 24 processors
+def parallel_filename_dict_from_tar_folder(path=None, n_jobs=-1):
+    if path is None:
+        path = os.getcwd()
+    tar_filenames = get_tar_filenames(path)
+    results = Parallel(n_jobs=n_jobs)(delayed(get_names)(tar_filename)
+                                      for tar_filename in tar_filenames)
+    return tar_filenames, results
+
+
+def get_names(tarfilename):
+    print 'scanning '+tarfilename
+    return tarfile.open(tarfilename).getnames()
